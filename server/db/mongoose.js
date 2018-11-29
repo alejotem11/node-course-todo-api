33c01efde3fbee6521f5f1ab79b98b5f36745249
Alejandro Tellez <alejotem_11@hotmail.com>
2018-11-29 11:07:29 -0500

    Setup for heroku and use the mongodb database deployed in https://mlab.com/databases/nodecourse-todoapi

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nodejs-course';
+const url = 'mongodb://todoapp1:todoapp1@ds121624.mlab.com:21624/nodecourse-todoapi'
+  || 'mongodb://localhost:27017/nodejs-course';
+  
 mongoose.Promise = global.Promise; // Set up mongoose to use Promises built-in javascript lib
 mongoose.connect(url, {useNewUrlParser: true });
 
