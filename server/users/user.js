36342a1a70912beafdb21b54d3091ea87bde8670
Alejandro Tellez <alejotem_11@hotmail.com>
2018-11-30 16:54:01 -0500

    Hash user password before saving

--- server/users/user.js
+++ server/users/user.js
@@ -5,10 +5,14 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const secret = 'somesecret'; // This shouldn't be clear in the code, instead it should be in the config file
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+// Used to salt the data when it is going to be encrypted
+// This shouldn't be clear in the code, instead it should be in the config file
+const secret = 'somesecret';
+
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -38,7 +42,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // Instance method
-userSchema.methods.generateAuthToken = function() {
+UserSchema.methods.generateAuthToken = function() {
   const user = this; // Instance
   const access = 'auth';
   const token = jwt.sign({ _id: user.id, access }, secret);
@@ -47,12 +51,12 @@ userSchema.methods.generateAuthToken = function() {
   return user.save().then(() => token);
 };
 
-userSchema.methods.toJSON = function() {
+UserSchema.methods.toJSON = function() {
   return _.pick(this, ['_id', 'email']);
 };
 
 // Model method
-userSchema.statics.findByToken = function (token) {
+UserSchema.statics.findByToken = function (token) {
   var User = this; // Object
   var decoded;
   try {
@@ -68,9 +72,26 @@ userSchema.statics.findByToken = function (token) {
   });
 };
 
+// Mongoose middleware to run before the save() method (instance method) is exectuted
+UserSchema.pre('save', function(next) {
+  const user = this;
+  // the method isModified is an instance method of the Schema object, and it
+  // takes only one property of the instance to check
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash;
+            next();
+        });
+    });
+  } else {
+    next();
+  }
+});
+
 // The first argument is the singular name of the collection your model is for.
 // Mongoose automatically looks for the plural version of your model name.
 // Thus, for the example below, the model User is for the users collection in the database
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
